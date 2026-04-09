ed433aff001fb3ad3122a07c1bd66294b1d6abce
kukur256 <bugaev2604@gmail.com>
2026-04-09 16:09:40 +0500

    api for fronted

--- src/orders/order_router.py
+++ src/orders/order_router.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends, APIRouter
 
 from src.database.session import get_db
-from src.orders.order_schemas import CreateOrderRequest, OrderItem
+from src.orders.order_schemas import CreateOrderRequest, OrderItem, GetOrdersResponse
 from src.enums import Status
 
 
@@ -26,19 +26,12 @@ def create_order(data: CreateOrderRequest, db: AsyncSession = Depends(get_db)):
     )
 
 @router.post("/department")
-def create_department(data: OrderItem, db: AsyncSession = Depends(get_db)):
-    return create_department(
-        id = uuid.uuid4(),
-        department_id = data.department_id
-    )
-
-@router.post("/get_orders")
-def get_orders(data: CreateOrderRequest, db: AsyncSession = Depends(get_db)):
-    return get_orders(
-        id=uuid.uuid4(),
+def change_department(data: OrderItem, db: AsyncSession = Depends(get_db)):
+    return OrderItem(
+        order_id=uuid.uuid4(),
         department_id=data.department_id,
         from_user_id=uuid.uuid4(),
-        worker=uuid.uuid4(),
+        worker_id=uuid.uuid4(),
         title=data.title,
         description=data.description,
         status=Status.NOT_STARTED,
@@ -46,17 +39,50 @@ def get_orders(data: CreateOrderRequest, db: AsyncSession = Depends(get_db)):
         finished_at=None
     )
 
+@router.post("/get_orders")
+def get_orders(data: CreateOrderRequest, db: AsyncSession = Depends(get_db)):
+    order = OrderItem(
+        order_id=uuid.uuid4(),
+        department_id=data.department_id,
+        from_user_id=uuid.uuid4(),
+        worker_id=uuid.uuid4(),
+        title=data.title,
+        description=data.description,
+        status=Status.NOT_STARTED,
+        created_at=datetime(2026, 3, 13, 17, 17, 17),
+        finished_at=None
+    )
+    return GetOrdersResponse(
+        items = [order],
+        page = 1,
+        length = 1
+
+    )
+
 @router.post("/change_status")
 def change_status(data: OrderItem, db: AsyncSession = Depends(get_db)):
-    return change_status(
-        id=uuid.uuid4(),
-        status=Status.NOT_STARTED
+    return OrderItem(
+        order_id=uuid.uuid4(),
+        department_id=data.department_id,
+        from_user_id=uuid.uuid4(),
+        worker_id=uuid.uuid4(),
+        title=data.title,
+        description=data.description,
+        status=Status.NOT_STARTED,
+        created_at=datetime(2026, 3, 13, 17, 17, 17),
+        finished_at=None
     )
 
 @router.post("/set_worker")
 def set_worker(data: OrderItem, db: AsyncSession = Depends(get_db)):
-    return set_worker(
-        id=uuid.uuid4(),
-        worker=uuid.uuid4(),
-        status=Status.NOT_STARTED
+    return OrderItem(
+        order_id=uuid.uuid4(),
+        department_id=data.department_id,
+        from_user_id=uuid.uuid4(),
+        worker_id=uuid.uuid4(),
+        title=data.title,
+        description=data.description,
+        status=Status.NOT_STARTED,
+        created_at=datetime(2026, 3, 13, 17, 17, 17),
+        finished_at=None
     )
